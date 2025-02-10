048998bde296874b5441690594565e0e6956599c
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-02-10 22:39:08 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+       {/* card.png is a file which containes a card Image. Write html
+    and css to design this card.use html  */}
+      <div class="box">
+        <div class="image">
+          <img height="230" src="pic.jpg" alt=""/>
+        </div>
+
+        <div class="capsules">
+          <span>Nature</span>
+          <span>forest</span>
+        </div>
+        <div class="content">
+          <h1>Guddiya</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At omnis
+            dolor corrupti impedit nostrum suscipit cumque voluptatem quas
+            provident animi,
+            minus nihil necessitatibus quibusdam error
+            dolorum asperiores accusantium, eius alias.</p>
+
+        </div>
+        <div class="button">
+          <button>Submit</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
